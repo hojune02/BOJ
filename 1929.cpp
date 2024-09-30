557986a6957791555f893b4fe37dca528b5e0e04
Hojune Kim <hojunekim1202@gmail.com>
2024-09-30 19:49:16 +0900

    소수 구하기

--- 1929.cpp
+++ 1929.cpp
@@ -1,13 +1,13 @@
 //백준 1929번: 소수 구하기
-/*
-Using Eratosthenes' Sieve
-*/
 #include <iostream>
 using namespace std;
 const int number = 1000000;
 int result[number+1];
 
 void isPrimeSieve(int x, int y){
+/*
+Using Eratosthenes' Sieve
+*/
     for (int i = 2; i <= y; i++){
         if (result[i] == 0) continue;
         for (int j = 2*i; j <= number; j += i){
